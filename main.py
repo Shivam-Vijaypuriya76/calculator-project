b760084338e3ba09de6154457e846b561e05c7ea
Rupesh yadav <rupeshyadav2022026@gmail.com>
2026-09-24 13:09:01 +0530

    Add multiplication feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from calculator import add, subtract
+from calculator import add, subtract, multiply
 
 
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10,5))
